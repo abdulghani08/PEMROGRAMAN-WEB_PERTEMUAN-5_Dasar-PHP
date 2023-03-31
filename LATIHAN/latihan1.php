d9e84551cbd0a6dac4b0cd0d329dbfb473e0396e
Muhammad Abdul Ghani <111698424+abdulghani08@users.noreply.github.com>
2023-03-31 22:52:58 +0700

    Add files via upload

---
+++ LATIHAN/latihan1.php
@@ -0,0 +1,11 @@
+<?php
+$hello = "Hello World!";
+$a_number = 4;
+$anotherNumber = 8.9;
+$bool = true;
+
+echo $hello; // Output: Hello World!
+echo $a_number; // Output: 4
+echo $anotherNumber; // Output: 8.9
+echo $bool; // Output: 1 (untuk nilai true)
+?>
